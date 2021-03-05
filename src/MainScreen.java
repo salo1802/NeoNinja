5c63fb1cf098c060d0fdc0d277c545d8e88ca514
Salo1802 <salo1802@hotmail.com>
2021-03-04 23:51:49 -0500

    Update MainScreen.java

--- src/MainScreen.java
+++ src/MainScreen.java
@@ -6,16 +6,28 @@ public class MainScreen {
 
 	private PImage p;
 	private PImage l;
+	private boolean subir;
+	private float y;
 	
 	public MainScreen(PApplet app) {
 	 p = app.loadImage("pantallaP.png");
 	 l = app.loadImage("LogoG.png");
+	 y = 110;
+	 subir = true;
 	}
 	
 	
 	public void PaintScreen(PApplet app) {
 		app.image(p, 500, 350);
-		int y = 210;
+		
+		if (subir == true) {
+			y += 0.5;}
+		if (subir == false) {
+			y -= 0.5;}
+		if (y == 100) {subir = true;}
+		if (y == 140) {subir= false;}
+			
+		
 		app.image(l,500,y);
 		
 	}
