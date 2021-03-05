bc79fdeb8888187e9cf158638d45eae455d91572
Salo1802 <salo1802@hotmail.com>
2021-03-04 23:19:02 -0500

    porfin funciona

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,43 @@
 
-public class Main {
+import processing.core.PApplet;
 
+public class Main extends PApplet{
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		PApplet.main("Main");
 	}
+	
 
-}
+	public void settings() {
+		size(1000,700);
+		
+		
+		 
+		 
+	}
+	
+	MainScreen p1;
+
+	
+	
+	
+	public void setup() {
+		imageMode(CENTER);
+		p1 = new MainScreen(this);
+	
+		  
+	}
+	
+
+public void draw() {
+	
+	p1.PaintScreen(this);
+	
+	
+	  
+     
+     
+
+     
+    
+	}}
--- src/MainScreen.java
+++ src/MainScreen.java
@@ -1,4 +1,22 @@
 
+import processing.core.PApplet;
+import processing.core.PImage;
+
 public class MainScreen {
 
+	private PImage p;
+	private PImage l;
+	
+	public MainScreen(PApplet app) {
+	 p = app.loadImage("pantallaP.png");
+	 l = app.loadImage("LogoG.png");
+	}
+	
+	
+	public void PaintScreen(PApplet app) {
+		app.image(p, 500, 350);
+		int y = 210;
+		app.image(l,500,y);
+		
+	}
 }
